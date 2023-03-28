2a4f42a6f0ba5de3a33330033a035e00283aa9cd
elsayed-hussein <elsayed.hussen@gmail.com>
2023-03-28 14:56:42 +0200

    finale

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-For-Beginners</title>
+</head>
+
+<body>
+    <ul>
+        <li>
+            <a href="./01_output.php">01_output</a>
+        </li>
+        <li>
+            <a href="./02_variables.php">02_variables</a>
+        </li>
+        <li>
+            <a href="./03_arrays.php">03_arrays</a>
+        </li>
+        <li>
+            <a href="./04_conditionals.php">04_conditionals</a>
+        </li>
+        <li>
+            <a href="./05_loops.php">05_loops</a>
+        </li>
+        <li>
+            <a href="./06_functions.php">06_functions</a>
+        </li>
+        <li>
+            <a href="./07_array_functions.php">07_array_functions</a>
+        </li>
+        <li>
+            <a href="./08_string_functions.php">08_string_functions</a>
+        </li>
+        <li>
+            <a href="./09_superglobals.php">09_superglobals</a>
+        </li>
+        <li>
+            <a href="./10_get_post.php">10_get_post</a>
+        </li>
+        <li>
+            <a href="./11_sanitizing_inputs.php">11_sanitizing_inputs</a>
+        </li>
+        <li>
+            <a href="./12_cookies.php">12_cookies</a>
+        </li>
+        <li>
+            <a href="./13_sessions.php">13_sessions</a>
+        </li>
+        <li>
+            <a href="./14_file_handling.php">14_file_handling</a>
+        </li>
+        <li>
+            <a href="./15_file_upload.php">15_file_upload</a>
+        </li>
+        <li>
+            <a href="./16_exceptions.php">16_exceptions</a>
+        </li>
+        <li>
+            <a href="./17_oop.php">17_oop</a>
+        </li>
+        <li>
+            <a href="./feedback/index.php">FeedBack</a>
+        </li>
+    </ul>
+</body>
+
+</html>
